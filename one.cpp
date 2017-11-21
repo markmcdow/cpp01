47430dba253c4c542eeb807c6459f48931a9dfbf
CORP\mmcdow <mmcdow@alkamitech.com>
2017-11-21 15:43:47 -0600

    Toying with VIM

--- one.cpp
+++ one.cpp
@@ -1,6 +1,18 @@
 #include <iostream>  
+
 using namespace std;  
+
 void main()  
-{  
-    cout << "Hello, world, from Visual C++!" << endl;  
+{
+	int totalx = 40;
+	int totaly = 20;
+	for(int y = 0;y <= totaly; y++) {
+		for(int x = 0;x <= totalx; x++)
+			if ((y == 0) || (x == 0) || (x == totalx) || (y == totaly) || ((2*y)==x) || ((2*y+x)==totalx))
+				cout << "o";
+			else
+				cout << " ";
+		cout << "\n";
+	}
 }  
+
